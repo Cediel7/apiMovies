b58f6aca2bf4daa559b2d059a4515e9a83b02c15
Cediel7 <davidcediel7@gmail.com>
2024-07-05 00:41:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/**
+ * Configuración del servidor Express con CORS y manejo de rutas para una API de medios.
+ * 
+ * Dependencias:
+ * - express: Framework web minimalista para Node.js.
+ * - cors: Middleware para habilitar CORS.
+ * - dotenv: Carga variables de entorno desde un archivo .env.
+ * 
+ * Rutas:
+ * - /api/movies: Maneja rutas relacionadas con películas.
+ * - /api/tv: Maneja rutas relacionadas con programas de televisión.
+ * - /api/people: Maneja rutas relacionadas con personas.
+ * - /api/trending: Maneja rutas relacionadas con contenidos en tendencia.
+ * 
+ * El servidor escucha en el puerto especificado en las variables de entorno.
+ */
+
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
@@ -14,18 +31,11 @@ dotenv.config()
 app.use(cors())
 app.use(express.json())
 
-//("/api/movies")
-//("/api/movies/:id")
-app.use('/api/movies', movies)
-//("/api/tv")
-//("/api/tv/:id")
-app.use('/api/tv', tv)
-//("api/people")
-//("api/people/:id")
-app.use('/api/people', people)
-//("/api/trending/:type")
-app.use('/api/trending', trending)
+app.use('/api/movies', movies)    // Rutas relacionadas con películas
+app.use('/api/tv', tv)            // Rutas relacionadas con programas de TV
+app.use('/api/people', people)    // Rutas relacionadas con personas
+app.use('/api/trending', trending) // Rutas relacionadas con contenidos en tendencia
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
+  console.log(El servidor está corriendo en el puerto ${process.env.PORT})
 })
